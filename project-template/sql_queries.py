a216a33c1eb560947e18b4ec20626ebe425b1a81
tajma <tajmasamantha@gmail.com>
2025-04-28 13:45:06 -0500

    Finished sql_queries.py, started etl.ipynb

--- project-template/sql_queries.py
+++ project-template/sql_queries.py
@@ -69,24 +69,39 @@ CREATE TABLE IF NOT EXISTS time (
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+values (%s, %s, %s,%s, %s, %s,%s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level) 
+VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
+
+
+
 
 # FIND SONGS
 
 song_select = ("""
+SELECT songs.song_id, songs.title
+FROM songs
 """)
 
 # QUERY LISTS
